2cbc0d891b3b95129a3cdea897422872771df558
JacobAmar <yaakov.amar@unity3d.com>
2025-01-04 17:54:23 +0200

    Implement resource reconciliation and deployment creation for Terragrunt resources

--- internal/controller/resource_controller.go
+++ internal/controller/resource_controller.go
@@ -18,8 +18,17 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
+	"time"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -47,10 +56,51 @@ type ResourceReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.2/pkg/reconcile
 func (r *ResourceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	resource := &terragruntv1alpha1.Resource{}
+	err := r.Get(ctx, req.NamespacedName, resource)
+	if err != nil {
+		if apierror.IsNotFound(err) {
+			log.Info("Terragrunt Resource not found")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get Terragrunt Resource")
+		return ctrl.Result{}, err
+	}
+	// Checking resource status condition
+	if resource.Status.Conditions == nil || len(resource.Status.Conditions) == 0 {
+		meta.SetStatusCondition(&resource.Status.Conditions, metav1.Condition{Type: "OutOfSync", Status: metav1.ConditionUnknown, Reason: "Reconciling", Message: "Starting reconciliation"})
+		if err := r.Status().Update(ctx, resource); err != nil {
+			log.Error(err, "Failed to update Terragrunt Resource status")
+			return ctrl.Result{}, err
+		}
+		if err := r.Get(ctx, req.NamespacedName, resource); err != nil {
+			log.Error(err, "Failed to re-fetch Terragrunt Resource")
+			return ctrl.Result{}, err
+		}
+	}
 
+	// Checking if the deployment is already running
+	found := &appsv1.Deployment{}
+	err = r.Get(ctx, types.NamespacedName{Name: resource.Name, Namespace: resource.Namespace}, found)
+	if err != nil && apierrors.IsNotFound(err) {
+		// Define a new deployment
+		dep, err := r.deploymentForTerragrunt(resource)
+		if err != nil {
+			log.Error(err, "Failed to create deployment")
+			return ctrl.Result{}, err
+		}
+		log.Info("Creating a new Deployment",
+			"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		if err = r.Create(ctx, dep); err != nil {
+			log.Error(err, "Failed to create new Deployment",
+				"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+		// Requeue Reconiliation
+		return ctrl.Result{RequeueAfter: time.Minute}, nil
+	}
 	return ctrl.Result{}, nil
 }
 
@@ -59,4 +109,47 @@ func (r *ResourceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&terragruntv1alpha1.Resource{}).
 		Complete(r)
+}
+
+func (r *ResourceReconciler) deploymentForTerragrunt(m *terragruntv1alpha1.Resource) (*appsv1.Deployment, error) {
+	// Define a new deployment for terragrunt resource
+	path := m.Spec.Path
+	replicas := int32(1)
+	// Testing
+	image := "ubuntu:22.04"
+	command := []string{"echo", "Path: ", path, "&&", "sleep", "3600"}
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-terragrunt-%s", m.Name, path),
+			Namespace: m.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": fmt.Sprintf("%s-terragrunt-%s", m.Name, path),
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": fmt.Sprintf("%s-terragunt-%s", m.Name, path),
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name:            fmt.Sprintf("%s-terragunt-%s", m.Name, path),
+						Image:           image,
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						Command:         command,
+					}},
+				},
+			},
+		},
+	}
+	if err := ctrl.SetControllerReference(m, deployment, r.Scheme); err != nil {
+		return nil, err
+	}
+	return deployment, nil
 }
