5e964b445a3f75131e7b30620e247ef0530fa10e
JacobAmar <yaakov.amar@unity3d.com>
2025-01-04 18:14:10 +0200

    Add RBAC permissions for pods in ResourceReconciler

--- internal/controller/resource_controller.go
+++ internal/controller/resource_controller.go
@@ -46,6 +46,7 @@ type ResourceReconciler struct {
 // +kubebuilder:rbac:groups=terragrunt.yaakov.com,resources=resources/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=terragrunt.yaakov.com,resources=resources/finalizers,verbs=update
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
